ba3e93de0705ab53e411145ddd47e1f6f8d31afd
DieG02 <43993554@alejandrovolta.edu.ar>
2021-03-22 21:49:17 -0300

    Categorias, scroll y estilos Hecho

---
+++ front-end/components/ListProducts.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { View, ScrollView, StyleSheet } from 'react-native'
+import Product from './Product.jsx'
+
+
+export default function ListProducts() {
+  const properties = [
+    {image: 'https://http2.mlstatic.com/D_NQ_NP_868738-MLA31322428821_072019-V.jpg', price: 1000, title: 'Hierro Perfil', description: '60 x 20 - 1.5mm'},
+    {image: 'https://ingcoecuador.com/wp-content/uploads/2020/04/uni.png', price: 560, title: 'Martillo Hierro', description: 'Martillo de orejas 80z'},
+    {image: 'https://http2.mlstatic.com/D_NQ_NP_868738-MLA31322428821_072019-V.jpg', price: 1000, title: 'Hierro Perfil', description: '60 x 20 - 1.5mm'},
+    {image: 'https://ingcoecuador.com/wp-content/uploads/2020/04/uni.png', price: 560, title: 'Martillo Hierro', description: 'Martillo de orejas 80z'}
+  ]
+
+  return(
+   <View style={styles.container}>
+      <ScrollView horizontal>
+        <View style={styles.products}>
+          {properties.map((props, index) => {
+            return( <Product properties={props} key={index}/> )
+          })}
+        </View>
+      </ScrollView>
+   </View>
+  )
+}
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    height: 250,
+    width: '100%',
+  },
+  products: {
+    height: '100%',
+    paddingLeft: 5,
+    paddingRight: 5,
+    flexDirection: 'row',
+  }
+})
--- front-end/screen/Home.jsx
+++ front-end/screen/Home.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 import TabBar from '../components/TabBar.jsx'
+import ListProducts from '../components/ListProducts.jsx'
 import { useNavigation } from '@react-navigation/native'
 
 function Home() {
   const navigation = useNavigation();
 
+
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Home Screen</Text>
+        <ListProducts/>
         <TabBar navigation={navigation}/>
       </View>
     )
