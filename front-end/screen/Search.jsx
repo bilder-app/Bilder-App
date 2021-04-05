c7debbdf829bb2358ee49e005e1542035884aa63
lucasmercado101 <lucasmercado101@gmail.com>
2021-04-05 10:56:10 -0300

    searchbar recent button changes

--- front-end/screen/Search.jsx
+++ front-end/screen/Search.jsx
@@ -1,87 +1,105 @@
-import React from 'react'
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
-import { useSelector } from 'react-redux'
+import React, { useState } from "react";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { useSelector, useDispatch } from "react-redux";
 
-import SearchBar from '../components/SearchBar.jsx'
+import SearchBar from "../components/SearchBar.jsx";
 
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
-import { faClock } from '@fortawesome/free-regular-svg-icons'
-import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons'
+import { unshiftHistory } from "../redux/actions/products.js";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { faClock } from "@fortawesome/free-regular-svg-icons";
+import { faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons";
 
+import { useNavigation } from "@react-navigation/native";
 
-export function HistoryItem({label}) {
+export function HistoryItem({ label, onClickText }) {
+  const navigation = useNavigation();
+  const dispatch = useDispatch();
+  const unshiftHistoryDispatched = (input) => dispatch(unshiftHistory(input));
 
-  return(
-    <TouchableOpacity 
-      style={styles.historyItem}
-      onPress={() => alert(`Llamar a search('${label}')`)}
-    >
-      <View style={styles.icons}>
-        <FontAwesomeIcon 
-          icon={faClock} 
-          color={'#AAA'} 
-          size={17}
-        />
-      </View>
-      <View style={styles.label}>
-        <Text>{label}</Text>
-      </View>
-       <View style={styles.icons}>
-        <FontAwesomeIcon 
-          icon={faExternalLinkAlt} 
-          color={'#AAA'} 
-          size={17}
-        />
-      </View>
-    </TouchableOpacity>
-  )
+  const onClickGoToRecentSearch = (labelName) => {
+    unshiftHistoryDispatched(labelName);
+    navigation.push("Results", labelName);
+  };
+
+  return (
+    <View style={styles.historyItem}>
+      <TouchableOpacity
+        style={styles.autocompleteText}
+        onPress={() => onClickText(label)}
+      >
+        <FontAwesomeIcon icon={faClock} color={"#AAA"} size={17} />
+        <View style={styles.label}>
+          <Text>{label}</Text>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => onClickGoToRecentSearch(label)}
+        style={styles.recentSearch}
+      >
+        <FontAwesomeIcon icon={faExternalLinkAlt} color={"#AAA"} size={17} />
+      </TouchableOpacity>
+    </View>
+  );
 }
 
 export default function Search() {
-  const history = useSelector(state => state.productsList.history);
+  const history = useSelector((state) => state.productsList.history);
+  const [searchQuery, setSearchQuery] = useState();
 
-  return(
+  return (
     <View style={styles.container}>
-      <SearchBar/>
+      <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
       <View style={styles.history}>
         {history.map((label, index) => {
-          return(<HistoryItem label={label} key={index}/>)
+          return (
+            <HistoryItem
+              onClickText={(text) => setSearchQuery(text)}
+              label={label}
+              key={index}
+            />
+          );
         })}
       </View>
     </View>
-  )
+  );
 }
-
 
 const styles = StyleSheet.create({
   container: {
-    width: '100%',
-    height: '100%',
+    width: "100%",
+    height: "100%"
   },
   history: {
-    height: 40,
-    width: '100%',
-    paddingHorizontal: 10,
+    display: "flex",
+    flex: 1,
+    width: "100%",
+    paddingHorizontal: 10
   },
   historyItem: {
-    height: '100%',
-    width: '100%',
+    display: "flex",
     borderBottomWidth: 0.5,
-    borderBottomColor: '#444',
-    flexDirection: 'row',
+    borderBottomColor: "#444",
+    flexDirection: "row"
   },
-  icons: {
-    width: '10%',
-    height: '100%',
-    justifyContent: 'center',
-    alignItems: 'center',
+  autocompleteText: {
+    paddingVertical: 8,
+    flexGrow: 1,
+    flex: 1,
+    display: "flex",
+    alignItems: "center",
+    flexDirection: "row",
+    paddingLeft: 8
   },
   label: {
-    width: '80%',
-    height: '100%',
-    paddingLeft: 5,
+    paddingLeft: 16,
     fontSize: 20,
-    color: '#707070',
-    justifyContent: 'center',
+    flexGrow: 1,
+    color: "#707070",
+    justifyContent: "center"
   },
-})
+  recentSearch: {
+    display: "flex",
+    justifyContent: "center",
+    paddingHorizontal: 8
+  }
+});
