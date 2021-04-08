27e40bd9b7046a25245cbaf96a16004394538957
lucasmercado101 <lucasmercado101@gmail.com>
2021-04-08 13:47:49 -0300

    added an ALL to text stories

--- front-end/storybook/stories/Text.stories.js
+++ front-end/storybook/stories/Text.stories.js
@@ -5,6 +5,22 @@ import CenterView from "./CenterView";
 
 storiesOf("Text", module)
   .addDecorator((getStory) => <CenterView>{getStory()}</CenterView>)
+  .add("All", () => (
+    <>
+      <Text variant="h1">h1. Heading</Text>
+      <Text variant="h2">h2. Heading</Text>
+      <Text variant="h3">h3. Heading</Text>
+      <Text variant="h4">h4. Heading</Text>
+      <Text variant="h5">h5. Heading</Text>
+      <Text variant="h6">h6. Heading</Text>
+      <Text variant="subtitle1">subtitle1. Sample Text</Text>
+      <Text variant="subtitle2">subtitle2. Sample Text</Text>
+      <Text variant="body1">body1. Sample Text</Text>
+      <Text variant="body2">body2. Sample Text</Text>
+      <Text variant="button">button. Sample Text</Text>
+      <Text variant="caption">caption. Sample Text</Text>
+    </>
+  ))
   .add("h1", () => <Text variant="h1">Sample Text</Text>)
   .add("h2", () => <Text variant="h2">Sample Text</Text>)
   .add("h3", () => <Text variant="h3">Sample Text</Text>)
