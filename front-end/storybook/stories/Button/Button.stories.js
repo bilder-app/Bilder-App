7502e365bc579ba775e81b5b3c67a4a60ac67300
DieG02 <43993554@alejandrovolta.edu.ar>
2021-04-09 14:39:11 -0300

    more examples

--- front-end/storybook/stories/Button/Button.stories.js
+++ front-end/storybook/stories/Button/Button.stories.js
@@ -9,11 +9,17 @@ import CenterView from '../CenterView';
 storiesOf('Button', module)
   .addDecorator((getStory) => <CenterView>{getStory()}</CenterView>)
   .add('default', () => (
-    <Button onPress={action('continue')} children={text('Large', 'Continuar')} />
+    <Button onPress={action('continue')} children='Continuar' />
   ))
   .add('medium secondary', () => (
-    <Button onPress={action('continue')} type='medium' color='secondary' children={text('Medium', 'Agregar más productos')} />
+    <Button onPress={action('continue')} children='Agregar más productos' type='medium' color='secondary' />
   ))
   .add('small success outline', () => (
-    <Button onPress={action('continue')} type='small' color='success' outline children={text('Small', 'Agregar al carrito')} />
+    <Button onPress={action('continue')} children='Agregar al carrito' type='small' color='success' outline />
+  ))
+  .add('large danger outline', () => (
+    <Button onPress={action('continue')} children='Eliminar' type='large' color='danger' outline />
+  ))
+  .add('small', () => (
+    <Button onPress={action('continue')} children='Agregar al carrito' type='small'/>
   ))
