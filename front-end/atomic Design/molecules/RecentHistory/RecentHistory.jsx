9073d85a76311ebd4e1ef80d5b85f43ca3c527d2
lucasmercado101 <lucasmercado101@gmail.com>
2021-05-20 15:10:31 -0300

    added proper props to recentHistory

--- front-end/atomic Design/molecules/RecentHistory/RecentHistory.jsx
+++ front-end/atomic Design/molecules/RecentHistory/RecentHistory.jsx
@@ -1,61 +1,50 @@
 import React from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, TouchableOpacity, Pressable } from "react-native";
+import Text from "../../atoms/Text/Text";
 import { faArrowRight, faClock } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 
-export default function RecentHistory() {
+export default function RecentHistoryItem({
+  content,
+  onTextClick,
+  onRedirectPress
+}) {
   return (
-    <View>
-      <View
+    <View
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        width: "100%",
+        backgroundColor: "#fff"
+      }}
+    >
+      <TouchableOpacity
+        onPress={onTextClick}
         style={{
           display: "flex",
           flexDirection: "row",
-          width: "100%",
-          marginBottom: 15
+          alignItems: "center",
+          flexGrow: 8,
+          paddingVertical: 12
         }}
       >
-        <TouchableOpacity
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-            flexGrow: 8
-          }}
-        >
-          <View style={{ marginHorizontal: 15 }}>
-            <FontAwesomeIcon size={20} icon={faClock} />
-          </View>
-          <Text>Clavos</Text>
-        </TouchableOpacity>
-        <View style={{ flexGrow: 1 }}>
-          <FontAwesomeIcon icon={faArrowRight} />
+        <View style={{ marginHorizontal: 15 }}>
+          <FontAwesomeIcon size={20} icon={faClock} />
         </View>
-      </View>
-      <View
+        <Text>{content}</Text>
+      </TouchableOpacity>
+      <Pressable
+        onPress={onRedirectPress}
         style={{
+          flexGrow: 2,
+          paddingVertical: 12,
           display: "flex",
-          flexDirection: "row",
-          width: "100%",
-          marginBottom: 15
+          justifyContent: "center",
+          alignItems: "center"
         }}
       >
-        <TouchableOpacity
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-            flexGrow: 8
-          }}
-        >
-          <View style={{ marginHorizontal: 15 }}>
-            <FontAwesomeIcon size={20} icon={faClock} />
-          </View>
-          <Text>Martillo</Text>
-        </TouchableOpacity>
-        <View style={{ flexGrow: 1 }}>
-          <FontAwesomeIcon icon={faArrowRight} />
-        </View>
-      </View>
+        <FontAwesomeIcon icon={faArrowRight} />
+      </Pressable>
     </View>
   );
 }
