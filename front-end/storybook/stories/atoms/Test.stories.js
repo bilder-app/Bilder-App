38a8369e755d62ce05c095903e480f4e8b94f399
billybautista <billy.bautista.cornelio@gmail.com>
2021-05-21 21:19:00 -0300

    Cambios

--- front-end/storybook/stories/atoms/Test.stories.js
+++ front-end/storybook/stories/atoms/Test.stories.js
@@ -1,13 +1,9 @@
 import * as React from "react";
 import { storiesOf } from "@storybook/react-native";
-import TabBar from "../../../atomic Design/organisms/TabBar";
+
 import { View } from "react-native";
 import CenterView from "../CenterView";
 
 storiesOf("Test atoms", module)
   .addDecorator((getStory) => <CenterView>{getStory()}</CenterView>)
-  .add("All", () => (
-    <View style={{ padding: 20 }}>
-      <TabBar />
-    </View>
-  ));
+  .add("All", () => <View style={{ padding: 20 }}></View>);
