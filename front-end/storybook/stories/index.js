bca84369425b09e185f425322253c200281e5fef
DieG02 <43993554@alejandrovolta.edu.ar>
2021-05-22 17:29:51 -0300

    Header done!

--- front-end/storybook/stories/index.js
+++ front-end/storybook/stories/index.js
@@ -1,13 +1,3 @@
-import "./FavoriteCard.stories";
-import "./OrderCard.stories";
-import "./SearchCard/SearchCard.stories";
-import "./OrderProductCard.stories";
-import "./CartProductCard.stories";
-import "./CardContainer/CardContainer.stories";
-import "./CategoryContainer/CategoryContainer.stories";
-import "./ScrollContainer/ScrollContainer.stories";
-import "./Card/Card.stories";
-
 // atoms
 import "./atoms/Button.stories";
 import "./atoms/Chip.stories";
@@ -24,11 +14,22 @@ import "./molecules/Tab.stories";
 
 // organisms
 import "./organisms/Search.stories";
+import "./organisms/Header.stories";
+
+
+
 
 import "./FavoriteCard.stories";
-import "./Home.stories";
-import "./Header.stories";
+import "./OrderCard.stories";
+import "./SearchCard/SearchCard.stories";
+import "./OrderProductCard.stories";
+import "./CartProductCard.stories";
+import "./CardContainer/CardContainer.stories";
+import "./CategoryContainer/CategoryContainer.stories";
+import "./ScrollContainer/ScrollContainer.stories";
+import "./Card/Card.stories";
 
+import "./Home.stories";
 import "./CategorySearch/CategorySearch.stories";
 import "./Pages/Cart.stories";
 import "./Pages/Orders.stories";
---
+++ front-end/storybook/stories/organisms/Header.stories.js
@@ -0,0 +1,32 @@
+import { storiesOf } from "@storybook/react-native";
+import React from "react";
+import Header from "../../../atomic Design/organisms/Header/Header.jsx";
+import CenterView from "../CenterView";
+
+
+storiesOf("Header", module)
+  .addDecorator((getStory) => <CenterView>{getStory()}</CenterView>)
+  .add("title", () => (
+    <Header 
+      children={{ text: "Carrito" }} 
+      variant="title" 
+    />
+  ))
+  .add("icons", () => (
+    <Header 
+      variant="icons" 
+      children={{ id: Math.floor((Math.random() * 100) + 1) }} 
+      onPress={{
+        favouriteAction: alert,
+        shareAction: console.log
+      }}
+    />
+  ))
+  // .add("medium styled", () => (
+  //   <Button
+  //     onPress={() => console.log("Iniciar sesión")} 
+  //     children="Iniciar sesión"
+  //     size="medium"
+  //     style={{ height: 40 }}
+  //   />
+  // ))
