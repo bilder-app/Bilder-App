704407d2a5eac605d21d6d365f9cbfc67fa55787
DieG02 <43993554@alejandrovolta.edu.ar>
2021-05-28 16:13:38 -0300

    search

--- front-end/storybook/stories/Pages/Search.stories.js
+++ front-end/storybook/stories/Pages/Search.stories.js
@@ -1,5 +1,5 @@
 import * as React from "react";
 import { storiesOf } from "@storybook/react-native";
-import Search from "../../../atomic Design/views/Search";
+import Search from "../../../atomic Design/pages/Search";
 
 storiesOf("Search template (p)").add("default", () => <Search />);
