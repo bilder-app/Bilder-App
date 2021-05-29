f3ca6b1007b1be03b3870ff443950ef36df9bc2c
DieG02 <43993554@alejandrovolta.edu.ar>
2021-05-28 22:07:31 -0300

    Cart Done!

--- front-end/atomic Design/molecules/ModalCart/ModalCart.jsx
+++ front-end/atomic Design/molecules/ModalCart/ModalCart.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, Input } from "react-native";
+import { View } from "react-native";
 import Button from  "../../atoms/Button/Button";
 import Text from "../../atoms/Text/Text";
 import { Entypo } from '@expo/vector-icons'; 
@@ -8,6 +8,7 @@ import { Entypo } from '@expo/vector-icons';
 export default function ModalCart({ onPress }) {
   const [value, setValue] = useState(1);
   const stock = 17;
+
   return(
     <View style={styles.default}>
       <Button 
@@ -15,9 +16,7 @@ export default function ModalCart({ onPress }) {
           value === 1 ? alert("Desea eliminar este producto del carrito?") :  setValue(value - 1)
         }} 
         style={styles.button} 
-        props={{
-          disabled: value <= 0
-        }} 
+        props={{ disabled: value <= 0 }} 
         outline 
       >
         <Entypo name="minus" size={17} color="#ff8000" />
@@ -28,9 +27,7 @@ export default function ModalCart({ onPress }) {
       <Button
         onPress={() => setValue(value + 1)}
         style={styles.button}
-        props={{
-          disabled: value === stock
-        }} 
+        props={{ disabled: value === stock }} 
         outline
       >
         <Entypo name="plus" size={17} color="#ff8000" />
@@ -45,7 +42,6 @@ const styles = {
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
-    // backgroundColor: "#0f0",
   },
   button: {
     width: 25,
--- front-end/atomic Design/pages/Cart.jsx
+++ front-end/atomic Design/pages/Cart.jsx
@@ -1,191 +1,114 @@
 import React from "react";
 import { View, FlatList, StyleSheet } from "react-native";
+
 import Header from "../organisms/Header/Header";
-import { faAngleLeft } from "@fortawesome/free-solid-svg-icons";
-import ProductCard from "../organisms/ProductCard/ProductCard";
+import CardItem from "../organisms/CardItem/CardItem";
 import Text from "../../atomic Design/atoms/Text/Text";
 import Button from "../../atomic Design/atoms/Button/Button";
 
+ 
+const random =  Math.floor((Math.random() * 100) + 1)
 const items = [
   {
-    id: 1,
-    title: "Listones de Madera",
-    shortDescription: "Medidas de 120 x 240",
-    total: 400,
-    singlePrice: 200,
-    inOrder: 2,
+    id: random,
+    image: "https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg",
+    description: "Muñeco de baby Joda coleccionable",
+    price: Math.floor((Math.random() * 1000) + 1),
   },
   {
-    id: 2,
-    title: "Listones de Madera",
-    shortDescription: "Medidas de 120 x 240",
-    total: 400,
-    singlePrice: 200,
-    inOrder: 2,
+    id: random + 1,
+    image: "https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg",
+    description: "Muñeco de baby Joda coleccionable",
+    price: Math.floor((Math.random() * 1000) + 1),
   },
   {
-    id: 3,
-    title: "Listones de Madera",
-    shortDescription: "Medidas de 120 x 240",
-    total: 400,
-    singlePrice: 200,
-    inOrder: 2,
+    id: random + 2,
+    image: "https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg",
+    description: "Muñeco de baby Joda coleccionable",
+    price: Math.floor((Math.random() * 1000) + 1),
   },
   {
-    id: 4,
-    title: "Listones de Madera",
-    shortDescription: "Medidas de 120 x 240",
-    total: 400,
-    singlePrice: 200,
-    inOrder: 2,
+    id: random + 3,
+    image: "https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg",
+    description: "Muñeco de baby Joda coleccionable",
+    price: Math.floor((Math.random() * 1000) + 1),
   },
   {
-    id: 5,
-    title: "Listones de Madera",
-    shortDescription: "Medidas de 120 x 240",
-    total: 400,
-    singlePrice: 200,
-    inOrder: 2,
+    id: random + 4,
+    image: "https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg",
+    description: "Muñeco de baby Joda coleccionable",
+    price: Math.floor((Math.random() * 1000) + 1),
   },
   {
-    id: 6,
-    title: "Listones de Madera",
-    shortDescription: "Medidas de 120 x 240",
-    total: 400,
-    singlePrice: 200,
-    inOrder: 2,
+    id: random + 5,
+    image: "https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg",
+    description: "Muñeco de baby Joda coleccionable",
+    price: Math.floor((Math.random() * 1000) + 1),
   },
   {
-    id: 7,
-    title: "Listones de Madera",
-    shortDescription: "Medidas de 120 x 240",
-    total: 400,
-    singlePrice: 200,
-    inOrder: 2,
-  },
-  {
-    id: 8,
-    title: "Listones de Madera",
-    shortDescription: "Medidas de 120 x 240",
-    total: 400,
-    singlePrice: 200,
-    inOrder: 2,
-  },
-  {
-    id: 9,
-    title: "Listones de Madera",
-    shortDescription: "Medidas de 120 x 240",
-    total: 400,
-    singlePrice: 200,
-    inOrder: 2,
-  },
-  {
-    id: 10,
-    title: "Listones de Madera",
-    shortDescription: "Medidas de 120 x 240",
-    total: 400,
-    singlePrice: 200,
-    inOrder: 2,
-  },
-  {
-    id: 11,
-    title: "Listones de Madera",
-    shortDescription: "Medidas de 120 x 240",
-    total: 400,
-    singlePrice: 200,
-    inOrder: 2,
-  },
-  {
-    id: 12,
-    title: "Listones de Madera",
-    shortDescription: "Medidas de 120 x 240",
-    total: 400,
-    singlePrice: 200,
-    inOrder: 2,
+    id: random + 6,
+    image: "https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg",
+    description: "Muñeco de baby Joda coleccionable",
+    price: Math.floor((Math.random() * 1000) + 1),
   },
 ];
 
-const renderItem = ({
-  item: { title, shortDescription, total, singlePrice, inOrder },
-}) => (
-  <View style={{ marginVertical: 10, marginLeft: "auto", marginRight: "auto" }}>
-    <ProductCard
-      title={title}
-      shortDescription={shortDescription}
-      total={total}
-      singlePrice={singlePrice}
-      inOrder={inOrder}
-    />
+const renderItem = ({ item }) => (
+  <View style={{ marginVertical: 5, marginHorizontal: 15 }} key={item.id}>
+    <CardItem variant="cart" children={item} onPress={alert}/>
   </View>
 );
 
 function Cart() {
   return (
-    <View style={styles.wrapper}>
-      <Header icon={faAngleLeft} title="Carrito" />
+    <View style={styles.main}>
+      <Header children={{ text: "Mi Carrito" }} style={{ elevation: 5 }}/>
+
       <FlatList
-        style={styles.list}
         data={items}
         renderItem={renderItem}
         keyExtractor={(item) => item.id}
       />
-      <View style={styles.totalContainer}>
-        <View style={styles.hr} />
-        <View style={styles.totalPrice}>
-          <Text>Subtotal</Text>
-          <Text style={styles.price} variant="h5">
-            $4400
-          </Text>
+ 
+      <View style={{ paddingHorizontal: 20 }}>
+        <View style={styles.hr}/>
+        <View style={styles.content}>
+          <Text style={{ color: "#707070" }} variant="subtitle2">Subtotal</Text>
+          <Text style={styles.price} variant="h6">$4400</Text>
         </View>
-        <Button
-          style={{
-            backgroundColor: "#878585",
-            marginLeft: "auto",
-            marginRight: "auto",
-            marginTop: 15,
-          }}
-        >
-          Agregar más products
-        </Button>
       </View>
-      <View style={styles.bottomButton}>
-        <Button text="Continuar" />
+
+      <View style={styles.button}>
+        <Button onPress={() => console.log("Continuar")} children="Continuar" />
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  wrapper: { height: "100%", display: "flex" },
-  list: { flexGrow: 0 },
-  totalPrice: {
-    display: "flex",
-    flexDirection: "row",
-    alignItems: "center",
-    marginTop: 10,
-  },
-  price: {
-    marginLeft: "auto",
-  },
-  totalContainer: {
-    marginBottom: "auto",
-    paddingHorizontal: 25,
+  main: { 
+    height: "100%",
+    width: "100%",
   },
   hr: {
-    paddingTop: 5,
-    borderBottomColor: "#a0a0a0",
-    borderBottomWidth: 1,
+    marginVertical: 10,
+    borderBottomColor: "#A0A0A0",
+    borderBottomWidth: 1.5,
     width: "90%",
     marginLeft: "auto",
     marginRight: "auto",
   },
-  bottomButton: {
-    marginRight: "auto",
-    marginLeft: "auto",
-    width: "90%",
-    paddingVertical: 15,
-    backgroundColor: "white",
-    marginTop: "auto",
+  content: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  button: {
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+    paddingVertical: 5,
+    backgroundColor: "transparent",
   },
 });
 
