0c7836962ad1a6df556913811748bb72ae401dbb
lucasmercado101 <lucasmercado101@gmail.com>
2021-06-08 15:36:44 -0300

    profile fetches with react query

--- atomic Design/pages/Profile.jsx
+++ atomic Design/pages/Profile.jsx
@@ -5,22 +5,34 @@ import Header from "../organisms/Header/Header";
 import Image from "../atoms/Image/Image";
 import Text from "../atoms/Text/Text";
 import { getMyUser } from "../../api";
+import { useQuery } from "react-query";
+import { useFocusEffect } from "@react-navigation/native";
 
 import { FontAwesome, MaterialCommunityIcons } from "@expo/vector-icons";
 
-export default function Profile() {
-  const fakeUser = {
-    photo:
-      "https://grandimageinc.com/wp-content/uploads/2015/09/icon-user-default.png",
-  };
+const fakeUser = {
+  photo:
+    "https://grandimageinc.com/wp-content/uploads/2015/09/icon-user-default.png"
+};
 
-  const [user, setUser] = useState();
-  useEffect(() => {
-    getMyUser().then((user) => {
-      setUser(user);
-      console.log(user);
-    });
-  }, []);
+export default function Profile() {
+  const {
+    data: userData = {},
+    isLoading,
+    refetch
+  } = useQuery("user data", getMyUser);
+
+  useFocusEffect(
+    React.useCallback(() => {
+      refetch();
+      // Do something when the screen is focused
+
+      return () => {
+        // Do something when the screen is unfocused
+        // Useful for cleanup functions
+      };
+    }, [])
+  );
 
   return (
     <View style={styles.default}>
@@ -34,10 +46,10 @@ export default function Profile() {
           style={{ borderRadius: 100 }}
         />
         <Text variant="h6">
-          {user ? `${user.name} ${user.lastname}` : "Cargando..."}
+          {isLoading ? "Cargando..." : `${userData.name} ${userData.lastname}`}
         </Text>
         <Text variant="subtitle1" style={{ color: "#707070" }}>
-          {user ? user.email : "Cargando..."}
+          {isLoading ? "Cargando..." : userData.email}
         </Text>
       </View>
 
@@ -56,14 +68,12 @@ export default function Profile() {
         <TouchableOpacity style={styles.buttons}>
           <FontAwesome name="question-circle" size={27} color="#444D52" />
           <Text variante="subtitle1" style={styles.text}>
-            {" "}
             Preguntas frecuentes
           </Text>
         </TouchableOpacity>
         <TouchableOpacity style={styles.buttons}>
           <FontAwesome name="power-off" size={27} color="#444D52" />
           <Text variante="subtitle1" style={styles.text}>
-            {" "}
             Cerrar sesión
           </Text>
         </TouchableOpacity>
@@ -77,19 +87,19 @@ const styles = StyleSheet.create({
     width: "100%",
     height: "100%",
     alignItems: "center",
-    backgroundColor: "#fff",
+    backgroundColor: "#fff"
   },
   info: {
     width: "100%",
     alignItems: "center",
     marginTop: 15,
-    marginTop: "5%",
+    marginTop: "5%"
   },
   menu: {
     width: "100%",
     alignItems: "center",
     justifyContent: "center",
-    marginTop: "20%",
+    marginTop: "20%"
   },
   buttons: {
     height: 45,
@@ -97,11 +107,11 @@ const styles = StyleSheet.create({
     marginVertical: 3,
     flexDirection: "row",
     alignItems: "center",
-    paddingLeft: "15%",
+    paddingLeft: "15%"
   },
   text: {
     color: "#444D52",
     paddingLeft: 10,
-    fontSize: 19,
-  },
+    fontSize: 19
+  }
 });
