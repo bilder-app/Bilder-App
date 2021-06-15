50cf18e9898afc84dbf8d603008aa0618e8da17c
Lucasmercado101 <lucasmercado101@gmail.com>
2021-06-15 17:28:45 -0300

    shipping shows totals numbers, and calculates delivery fee

--- atomic Design/pages/Shipping.jsx
+++ atomic Design/pages/Shipping.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { View, StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, StyleSheet, Switch } from "react-native";
 
 import Header from "../organisms/Header/Header";
 import Text from "../atoms/Text/Text";
@@ -9,13 +9,57 @@ import ScrollContainer from "../atoms/ScrollContainer/ScrollContainer";
 import { useQuery } from "react-query";
 import { getCheckoutCartProducts } from "../../api";
 
+const getTotalPriceOfProducts = (products) =>
+  products.reduce((acc, { units, price }) => {
+    return acc + units * price;
+  }, 0);
+
 export default function Shipping({ navigation }) {
+  const [checkoutDetails, setCheckoutDetails] = useState({});
   const { data, isLoading } = useQuery(
     "checkout cart products",
     getCheckoutCartProducts
   );
 
-  if (isLoading) return null;
+  useEffect(() => {
+    if (data) {
+      data.forEach(({ business }) => {
+        setCheckoutDetails((prev) => {
+          const newObj = { ...prev };
+          newObj[business.id] = {
+            delivery: business.delivery // delivery === true, takeAway === false
+          };
+          return newObj;
+        });
+      });
+    }
+  }, [data]);
+
+  const toggleSwitch = (businessId) => {
+    setCheckoutDetails((prev) => {
+      const newObj = { ...prev };
+      newObj[businessId].delivery = !prev[businessId].delivery;
+      return newObj;
+    });
+  };
+
+  if (!Object.keys(checkoutDetails).length) return null;
+
+  const subtotal = data.reduce((total, { products }) => {
+    return (
+      total + products.reduce((acc, next) => acc + next.units * next.price, 0)
+    );
+  }, 0);
+
+  const deliveryCost = data.reduce((acc, { business, products }) => {
+    const productsTotal = getTotalPriceOfProducts(products);
+    const isDelivering = checkoutDetails[business.id].delivery;
+    if (isDelivering && productsTotal < business.freeDeliveryAt)
+      return acc + business.deliveryPrice;
+    else return acc;
+  }, 0);
+
+  const finalTotalCost = subtotal + deliveryCost;
 
   return (
     <View style={styles.main}>
@@ -39,7 +83,7 @@ export default function Shipping({ navigation }) {
               </View>
             </View>
             {data.map(({ packageNumber, products, business }) => (
-              <View style={{ marginBottom: 20 }}>
+              <View key={business.id} style={{ marginBottom: 20 }}>
                 <Text variant="h4">Paquete {packageNumber}</Text>
                 <View
                   style={{
@@ -48,29 +92,50 @@ export default function Shipping({ navigation }) {
                     justifyContent: "center",
                     padding: 10,
                     borderRadius: 10,
-                    marginTop: 5
+                    marginTop: 5,
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "space-between"
                   }}
                 >
-                  <Text variant="h5">Envio a domicilio</Text>
+                  <Text variant="h5">
+                    {business.delivery
+                      ? "Envio a domicilio"
+                      : "Retiro en el local"}
+                  </Text>
+                  <Switch
+                    onValueChange={() => toggleSwitch(business.id)}
+                    value={checkoutDetails[business.id].delivery}
+                  />
                 </View>
-                {products.map((productData) => (
+                {products.map((productData, i) => (
                   <CardItem
+                    key={i}
                     variant="shippingDetail"
                     children={{ ...productData, images: [productData.image] }}
                     onPress={console.log}
                   />
                 ))}
-                <View
-                  style={{
-                    flexDirection: "row",
-                    justifyContent: "space-between",
-                    alignItems: "center",
-                    backgroundColor: "white"
-                  }}
-                >
-                  <Text variant="h3">Costo de Envio</Text>
-                  <Text variant="h3">$1000</Text>
-                </View>
+                {business.deliveryPrice &&
+                  checkoutDetails[business.id].delivery && (
+                    <View
+                      style={{
+                        flexDirection: "row",
+                        justifyContent: "space-between",
+                        alignItems: "center",
+                        backgroundColor: "white"
+                      }}
+                    >
+                      <Text variant="h3">Costo de Envio</Text>
+                      <Text variant="h3">
+                        $
+                        {getTotalPriceOfProducts(products) <
+                        business.freeDeliveryAt
+                          ? 0
+                          : business.deliveryPrice}
+                      </Text>
+                    </View>
+                  )}
               </View>
             ))}
 
@@ -80,7 +145,7 @@ export default function Shipping({ navigation }) {
                   Subtotal
                 </Text>
                 <Text style={styles.price} variant="h6">
-                  $ 0
+                  $ {subtotal}
                 </Text>
               </View>
               <View style={styles.header}>
@@ -88,7 +153,7 @@ export default function Shipping({ navigation }) {
                   Costo de Entrega Total
                 </Text>
                 <Text style={styles.price} variant="h6">
-                  $ 0
+                  $ {deliveryCost}
                 </Text>
               </View>
               <View style={styles.header}>
@@ -96,7 +161,7 @@ export default function Shipping({ navigation }) {
                   Total
                 </Text>
                 <Text style={styles.price} variant="h6">
-                  $ 0
+                  $ {finalTotalCost}
                 </Text>
               </View>
             </View>
